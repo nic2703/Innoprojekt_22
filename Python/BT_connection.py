7e29543afa128b2005a95de9d1eb84acb92f9fba
TheSmilingTurtle <vojta.kottas@gmail.com>
2022-03-21 20:54:17 +0100

    added commecnts to the python file

--- Python/BT_connection.py
+++ Python/BT_connection.py
@@ -1,10 +1,11 @@
 import socket
 
-address = "4C:87:5D:D0:1A:00"    
+address = "4C:87:5D:D0:1A:00" #define address
+#better way would be to scan for addresses, look into that if possible
 
 def test(port):
     try:
-        sock.connect((address, port))
+        sock.connect((address, port)) #attempt connection
         print("Connected to", address, "using port", port)
         return True
     except:
@@ -12,8 +13,10 @@ def test(port):
         return False
 
 
-sock = socket.socket(socket.AF_BLUETOOTH, socket.SOCK_STREAM, socket.BTPROTO_RFCOMM)
+sock = socket.socket(socket.AF_BLUETOOTH, socket.SOCK_STREAM, socket.BTPROTO_RFCOMM) #initialize sock as a BT socket
 
-for p in range(1000): #check the first 1000 ports
-    if test(p):
+for p in range(10): #check the first 10 ports
+    if test(p): #if the connection could be initialized, great
         break
+
+#sock.sendfiile() #needs investigation, alternatively test with sock.send()
