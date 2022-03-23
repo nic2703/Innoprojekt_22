d5ef26bb42fcc37d36df3a0648a4d7938ec82a0e
Nic <you@example.com>
2022-03-23 15:55:08 +0100

    plotter class works well:D

--- Arduino/Protothreading Testing/MT_header.h
+++ Arduino/Protothreading Testing/MT_header.h
@@ -14,8 +14,9 @@ class Motor{
     public:
         Motor(pin pspeed, pin pbreak, pin pdirection, float position = 0.0f, float r = 10.0f);
         ~Motor();
-        bool movecoord(float posnew, float speed);
-        bool movecoord(float distance, float speed);
+        bool movecoord(float posnew, float speed = 100.0f);
+        bool movedistance(float distance, float speed = 100.0f);
+        bool startwspeed(float speed = 100.0f, bool inmms = true);
         pin pinspeed() const{
             return pspd;
         }
@@ -63,13 +64,44 @@ Motor::Motor(pin pspeed, pin pbreak, pin pdirection, float position, float r){
 Motor::~Motor(){}
 
 bool Motor::movecoord(float posnew, float speed){
+    speed>0?:speed=-speed;
     bitvalspd = map(speed/(2*PI*radius), 20, 60, 30, 255);
+    if (bitvalspd < 30 || bitvalspd > 255){
+        bitvalspd = 100.0f;
+        return false;
+    }
     posnew>pos?digitalWrite(pdir, HIGH):digitalWrite(pdir, LOW);
     float distance = posnew > pos ? posnew-pos:pos-posnew;
+
+    float time = distance/(2*PI*radius*(8.0f/15.0f*bitvalspd+15));
+    digitalWrite(pbrk, LOW);
+    analogWrite(pspd, bitvalspd);
+    delay(1000*time);
+
+    digitalWrite(pbrk, HIGH);
+    pos = posnew;
+    return true;
+    
     //basically, set two variables to positions and the time for the motors to run at a specific speed (NO CURVES YET)
     //the PT_header file checks these two variables sequentially and keeps the motors on for 1 second, taking away 1000ms from the time variable
     //this means that all motor move commands will be in the PT_header.h file
+}
 
+//Motor::movedirection missing here, will add -Nic
+
+bool Motor::startwspeed(float speed, bool inmms){
+    speed > 0 ? digitalWrite(pdir, HIGH):digitalWrite(pdir, LOW), speed = -speed;       //if speed is a negative value, switch direction of the motor
+    if (inmms){
+        bitvalspd = map(speed/(2*PI*radius), 20, 60, 30, 255);
+    } else {
+        bitvalspd = speed;
+    }
+    if (bitvalspd < 30 || bitvalspd > 255){
+        bitvalspd = 100.0f;
+        return false;
+    }
+    digitalWrite(pbrk, LOW);                //release the handbrake
+    analogWrite(pspd, bitvalspd);           //start moving at the required speed
 }
 
 
--- Arduino/Protothreading Testing/PT_header.h
+++ Arduino/Protothreading Testing/PT_header.h
@@ -1,18 +1,37 @@
 #ifndef PT_H_
 #define PT_H_
 #include "TimedAction.h"
+#include <HardwareSerial.h>
 
+bool moveXfor(int ms);
+bool moveYfor(int ms);
+
+unsigned long int timer = 0;
 //things to be protothreaded are initialized as TimedAction objects
-
-TimedAction MXAction = TimedAction(1000, MotorX);       //every 1 second
+TimedAction MXAction = TimedAction(1000, MotorX);       //every 1 second check
 TimedAction MYAction = TimedAction(1000, MotorY);
 
+void setup(){
+    Serial.begin(9600);
+    moveXfor(3000);
+    
+}
+
 void loop(){
+    Serial.println("Iteration #");
+    Serial.print(timer);
+    Serial.print("  ");
+    Serial.print(millis());
+    Serial.print("ms");
     MXAction.check();
     MYAction.check();
 }
 
 void MotorX(){
+    
+}
+
+void MotorY(){
     
 }
 
