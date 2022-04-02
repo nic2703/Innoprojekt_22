74ae66b1d1119ea4fa206c17481a9ee885701454
Nic <you@example.com>
2022-04-02 19:51:27 +0200

    good progress on the iterating

--- Windows/cpp/svgparsing/svgparsing2.cpp
+++ Windows/cpp/svgparsing/svgparsing2.cpp
@@ -6,21 +6,83 @@ using namespace std;
 ifstream fin;
 ofstream fout;
 
+bool searchforpath();
+bool ganalysis();
+bool pathanalysis();
+
 bool searchforpath(){
     string tempstr;
-    int wordnum = 0;
-    while (fin >> tempstr){
-        if (fin.eof()){
-            cout << "END OF FILE" << endl;
-            return false;
-        } else if (fin.fail()){
-            cout << "INPUT TERMINATED DATA MISMATCH" << endl;
-            return false;
-        } else {
-            wordnum++;
-            cout << tempstr << endl;
+    int counter = 0;
+    while (fin>>tempstr){
+        counter++;
+        if (tempstr == "<g" || tempstr == "<g>"){
+            cout << "G FOUND AT STR " << counter << endl;
+            ganalysis();
         }
     }
+    return true;
+}
+
+bool ganalysis(){
+    string tempstr;
+    int counter = 0;
+    while (fin>>tempstr){
+        counter++;
+        if (tempstr == "<path"){
+            cout << "PATH FOUND AT STR " << counter << endl;
+            pathanalysis();
+        }
+    }
+    return true;
+}
+
+bool pathanalysis(){
+    char ch;
+    while (fin>>ch && ch!='\"'){};              //skips until the actual line starts
+    string pairx[4];
+    string pairy[4];
+    string temp;
+    char command;
+    int i = 0;                                  //current index for the x-y list
+    bool firstnum = true;
+    while (fin>>ch && ch!='\"'){
+        if ((ch>='0' && ch<='9') || ch=='.'){
+            temp.push_back(ch);
+        }
+        if (ch==',' || ch==' '){
+            if (firstnum == true){
+                pairx[i] = temp;
+                temp = "";
+                firstnum = false;
+            } else {
+                pairy[i] = temp;
+                i++;
+                temp = "";
+                firstnum = true;
+            }
+        }
+        if (ch == 'C' || ch == 'c' || ch == 'S' || ch == 's'){
+            if (pairx[i]!="\0"){
+                pairy[i] = temp;
+                i++;
+                temp = "";
+                firstnum = true;
+            }
+            if (i == 4){
+                //CUBIC BEZIER
+                cout << command << " ";
+                for (int i = 0; i < 4; i++){
+                    cout << pairx[i] << " " << pairy[i] << " " << endl;
+                }
+            }
+            command = ch;
+        }
+
+
+
+
+    }
+
     return true;
 }
 
