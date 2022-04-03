d0ce3407a71ce061e632abf65d542e1786456420
VikD <damanivikram1@gmail.com>
2022-04-03 20:09:23 +0200

    modified constructor of PLOTTER

--- Arduino/DRIVERS/PT_motor.ino
+++ Arduino/DRIVERS/PT_motor.ino
@@ -1,58 +1,44 @@
 #include <MT2_header.h>
 
-# define brakePin1 8
-# define brakePin2 9
-
 void setup() {
-
-  //Mordor A
-  pinMode(12, OUTPUT); // direction pin 
-  pinMode(brakePin2, OUTPUT); // brake pin
   //speedpin of motor A is pin 3
   Serial.begin(9600);
-
-  //Mordor B
-  pinMode(13, OUTPUT); // direction pin
-  pinMode(brakePin1, OUTPUT); // brake pin
-  //speedpin of motor B is 11
-  
-  set_brakes(brakePin1, HIGH); //engage both brakes
-  set_brakes(brakePin2, HIGH); 
-
-    /***********************************************
-     *                                             *
-     *   code for individual and combinerd test    *
-     *                                             *
-     * *********************************************/
+ 
+  /***********************************************
+   *                                             *
+   *   code for individual and combinerd test    *
+   *                                             *
+   * *********************************************/
     Plotter plotter = Plotter(3.2, 6.5); // there exists an object
-
-    }
+    
+}
 
 void dualMotorTest() {
     const int SPEEDBIT = 255;
 
-    set_brakes(brakePin2, LOW);
-    set_speed(3, 12, HIGH, SPEEDBIT); // speed motor a to 180
+    set_brakes(_BRAKE_A, LOW);
+    set_dir(12, HIGH);
+    set_speed(3, 12, HIGH, SPEEDBIT); // speed motor a to 255
     delay(2000);
 
-    set_brakes(brakePin2, HIGH);
+    set_brakes(_BRAKE_A, HIGH);
 
-    set_brakes(brakePin1, LOW);
+    set_brakes(_BRAKE_B, LOW);
     set_speed(11, 13, LOW, SPEEDBIT);
 
     delay(2000);
 
-    set_brakes(brakePin1, HIGH);
+    set_brakes(_BRAKE_B, HIGH);
 
-    set_brakes(brakePin1, LOW);
-    set_brakes(brakePin2, LOW);
+    set_brakes(_BRAKE_B, LOW);
+    set_brakes(_BRAKE_A, LOW);
     set_speed(3, 12, HIGH, SPEEDBIT);
     set_speed(3, 12, HIGH, SPEEDBIT);
 
     delay(2000);
 
-    set_brakes(brakePin1, HIGH);
-    set_brakes(brakePin2, HIGH); 
+    set_brakes(_BRAKE_B, HIGH);
+    set_brakes(_BRAKE_A, HIGH); 
 }
 
 void loop(){
