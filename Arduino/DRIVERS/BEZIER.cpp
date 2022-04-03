18d866956b7a7c7c144ced953b6c2831963366f9
VikD <damanivikram1@gmail.com>
2022-04-03 22:19:04 +0200

    fixed comment

--- Arduino/DRIVERS/BEZIER.cpp
+++ Arduino/DRIVERS/BEZIER.cpp
@@ -2,7 +2,7 @@
 #include "MT2_header.h"
 #include "BEZIER_H.h"
 
-// general formula is: bB(t = (1-t)^3*p_0 + 3*(1-t)^2*t*p1 + 3*(1-t)*t*p_2+t^3*p_3, 0<t<1
+// general formula is: B(t) = (1-t)^3*p_0 + 3*(1-t)^2*t*p1 + 3*(1-t)*t*p_2+t^3*p_3, 0<t<1
 
 class BEZIER : public Plotter
 {
