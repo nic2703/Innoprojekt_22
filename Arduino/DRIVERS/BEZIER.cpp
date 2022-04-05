b40b4a524eecc2689c0da912e4e327120aed11b5
VikD <damanivikram1@gmail.com>
2022-04-05 22:23:09 +0200

    basic curve

--- Arduino/DRIVERS/BEZIER.cpp
+++ Arduino/DRIVERS/BEZIER.cpp
@@ -18,7 +18,26 @@ void BEZIER::_init_coords(){
     curve.y = 0;
 }
 
-bool BEZIER::robotica_test(){
-    Plotter::draw_line();
+
+//should plot an r
+bool BEZIER::robotica_test()
+{
+    Plotter::draw_line(0, 100, 255);
+    Plotter::draw_line(20, 0, 255);
+    Plotter::draw_line(0, -65, 255);
+    Plotter::draw_line(10, 10, 255);
+    Plotter::draw_line(10, 5, 255);
+    Plotter::draw_line(25, 0, 255);
+    Plotter::draw_line(10, -5, 255);
+    Plotter::draw_line(10, -10, 255);
+    Plotter::draw_line(15, 15, 255);
+    Plotter::draw_line(-10, 10, 255);
+    Plotter::draw_line(-20, 7, 255);
+    Plotter::draw_line(-25, 0, 255);
+    Plotter::draw_line(-20, -7, 255);
+    Plotter::draw_line(-3, -3, 255);
+    Plotter::draw_line(0, 10, 255);
+    Plotter::draw_line(-20, 0, 255);
+    Plotter::draw_line(0, -50, 255);
 }
 
